c3b69a4a7ee6bf5463187577e3ddd4f8242f8576
L525 <122877453@>
2020-12-27 17:10:23 +0800

    1、添加product.xlsx 2、添加Product.java 3、添加ReadProductExcel

---
+++ src/Product.java
@@ -0,0 +1,39 @@
+public class Product {
+
+    private String id;
+    private String name;
+    private Float price;
+    private String decs;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDecs() {
+        return decs;
+    }
+
+    public void setDecs(String decs) {
+        this.decs = decs;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,32 @@
-import java.io.File;
 import java.util.Scanner;
 
-public class Test{
-    public static void main(String[] args){
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException {
 
-        File file=new File("C:\\Users\\lenovo\\IdeaProjects\\cmdShop\\src\\users.xlsx");
-        ReadExcel readExcel=new ReadExcel();
-        User users[]=readExcel.readExcel(file);
+        boolean bool = true;
+        while (bool) {
 
-        System.out.println("please input username:");//提示用户输入用户名
-        Scanner sc =new Scanner(System.in);
-        String username=sc.next();
+            java.io.InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            ReadUserExcel readExcel = new ReadUserExcel();
+            User users[] = readExcel.readExcel(in);
 
-        System.out.println("please input password:");//提示用户输入密码
-        String password=sc.next();
+            System.out.println("please input username:");//提示用户输入用户名
+            Scanner sc = new Scanner(System.in);
+            String username = sc.next();
 
-        for (User user:users){
-            if(username.equals(user.getUsername()) && password.equals(user.getPassword())){
-                System.out.println("登录成功");
+            System.out.println("please input password:");//提示用户输入密码
+            String password = sc.next();
+
+            for (int i=0;i<users.length;i++) {
+                if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                    System.out.println("登录成功");
+                    bool = false;
+                    break;
+                } else {
+                    System.out.println("登录失败");
+                }
+
             }
-
         }
     }
 }
