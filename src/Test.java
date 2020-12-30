4c388066c42b71a6b8d76f60757021f45e7ac0af
L525 <122877453@qq.com>
2020-12-30 20:58:43 +0800

    1、按1继续购买 2、按2结账

--- src/Test.java
+++ src/Test.java
@@ -39,15 +39,61 @@ public class Test {
                     inProduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
                     Product product=readProductExcel.getProductById(Id,inProduct);
                     if(product!=null){
-
+                        carts[count++]=product;
                     }
-                    else if(choose==2){
-                        for (Product carts:products) {
-                            System.out.print(carts.getId());
-                            System.out.print("\t" + carts.getName());
-                            System.out.print("\t" + carts.getPrice());
-                            System.out.println("\t" + carts.getDecs());
+                    System.out.println("继续购买请按1");
+                    System.out.println("查看购物车请按2");
+                    int choose=sc.nextInt();
+                    if(choose==1){
+                        for (Product product1:products) {
+                            System.out.print(product1.getId());
+                            System.out.print("\t" + product1.getName());
+                            System.out.print("\t" + product1.getPrice());
+                            System.out.println("\t" + product1.getDecs());
+                    }
+                        System.out.println("请输入商品ID吧该商品加入购物车");
+                        Id=sc.next();
+                        inProduct=null;
+                        inProduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
+                        readProductExcel=new ReadProductExcel();
+                        product=readProductExcel.getProductById(Id,inProduct);
+                        if(product!=null){
+                            carts[count++]=product;
+                        }
+                }
+                    else if(choose==2) {
+                        System.out.println("当前购物车商品如下");
+                        for (Product p : carts) {
+                            if (p != null) {
+                                System.out.print(p.getId());
+                                System.out.print("\t" + p.getName());
+                                System.out.print("\t" + p.getPrice());
+                                System.out.println("\t" + p.getDecs());
+                            }
+                        }
+                        System.out.println("继续购买请按1");
+                        System.out.println("结账请按2");
+                        choose=sc.nextInt();
+                        if(choose==1){
+                            for (Product product1:products) {
+                                System.out.print(product1.getId());
+                                System.out.print("\t" + product1.getName());
+                                System.out.print("\t" + product1.getPrice());
+                                System.out.println("\t" + product1.getDecs());
+                            }
+                            System.out.println("请输入商品ID吧该商品加入购物车");
+                            Id=sc.next();
+                            inProduct=null;
+                            inProduct = Class.forName("Test").getResourceAsStream("/products.xlsx");
+                            readProductExcel=new ReadProductExcel();
+                            product=readProductExcel.getProductById(Id,inProduct);
+                        }
+                        else if(choose==2){
+                            System.out.println("你输入支付" + product.getPrice() + "元");
+                            System.out.println("请输入你的地址");
+                            System.out.println("请输入你的电话号码");
 
+                        }
                     }
 
 
